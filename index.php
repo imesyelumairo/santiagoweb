b77735e69a5f185d088efee60a21ab1a8e58a017
imesyelumairo <imesyelumairo@gmail.com>
2021-11-05 07:49:22 +0700

    Membuat web sedarhana

---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Kelurahan Santiago</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Anyar - v4.6.0
+  * Template URL: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+      </div>
+      <div class="cta d-none d-md-block">
+        <a href="#about" class="scrollto">Selamat Datang</a>
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="index.html">KELURAHAN SANTIAGO</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href=index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#about">Profil Kelurahan</a></li>
+          <li><a class="nav-link scrollto" href="#team">List Data Pegawai</a></li>
+          
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex justify-cntent-center align-items-center">
+    <div id="heroCarousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
+
+      <!-- Slide 1 -->
+      <div class="carousel-item active">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Selamat Datang di website Kelurahan Santiago <span>Kecamatan Tahuna</span></h2>
+          <p class="animate__animated animate__fadeInUp">Kelurahan Santiago Kecamatan Tahuna, Kabupaten Kepulauan Sangihe, Sulawesi Utara</p>
+          <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Selanjutnya</a>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bx bx-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bx bx-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Icon Boxes Section ======= -->
+    <section id="icon-boxes" class="icon-boxes">
+      <div class="container">
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Icon Boxes Section -->
+
+    <!-- ======= Profil Kelurahan ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Profil Kelurahan</h2>
+          <p></p>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <p>
+              Visi
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i>"Kepulauan SANGIHE Daerah Perbatasan NKRI, Sebagai
+                Gerbang Meritim Indonesia yang maju, Sejahtera, dan Mandiri" </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Misi (SAPTA KARYA)
+              </p>
+              <ul>
+                <li><i class="ri-check-double-line"></i>1. MENURUNKAN TINGKAT KEMISKINAN MASYARAKAT SECARA SISTEMATIS, TERPADU, DAN KONSISTEN;</li>
+                <li><i class="ri-check-double-line"></i>2. MEMBANGUN PEMERINTAHAN MELAYANI RAKYAT, HANDAL DENGAN TATA KELOLA
+                YANG EFEKTIF, EFISIEN, AKUNTABEL DIJALANKAN APARATUR PENYELENGGARA BERINTEGRITAS, PROFESIONAL, BEBAS KORUPSI, KOLUSI, DAN NEPOTISME; </li>
+                <li><i class="ri-check-double-line"></i>3. MEMBANGUN INFRASTRUKTUR DAERAH SECARA TEPAT GUNA BERDASARKAN RENCANA TATA RUANG WILAYAH
+                YANG KONSISTEN, BERKELANJUTAN DAN BERWAWASAN LINGKUNGAN;</li>
+                <li><i class="ri-check-double-line"></i>4. MENINGKTAN KONEKTIVITAS DAN KONTIUNITAS TRANSPORTASI WILAYAH, TELEKOMUNIKASI DAN TEKNOLOGI
+                INFORMASI SEBAGAI DAERAH PERBATASAN DAN PENJAGA WILAYAH KEDAULATAN NKRI; </li>
+                <li><i class="ri-check-double-line"></i>5. MEMBANGUN KEMANDIRIAN EKONOMI DAERAH BERBASIS KERAKYATAN SECARA TERENCANA, SISTEMATIS DAN BERKELANJUTAN; </li>
+                <li><i class="ri-check-double-line"></i>6. MEWUJUDKAN KEHIDUPAN MASYARAKAT YANG RELIGIUS, NASIONALIS, TANGGUH DAN BERDAYA SAING YANG BERDASAR PADA
+                JATI DIRI BUDAYA DAN KEARIFAN LOKAL;</li>
+                <li><i class="ri-check-double-line"></i>7. MENGELOLAH SUMBER DAYA ALAM DAN LINGKUNGAN HIDUP BERBASIS MASYARAKAT, SECARA TERPADU, KOMPREHENSIF DAN BERKELANJUTAN.</li>
+                  
+                  
+              </ul>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+    </section><!-- End Cta Section -->
+
+    <!-- ======= List Data Pegawai ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Kepegawaian</h2>
+          <p>Susunan Kepengurusan Kantor Lurah Santiago</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Zaiful H. Markus, SE</h4>
+                <span>19741105 199803 1 003</span>
+                <p>LURAH SANTIAGO</p>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Marlina Towoliu, A.AP</h4>
+                <span>NIP.19760529 200003 2 003</span>
+                <p>SEKRETARIS KELURAHAN</p>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>-</h4>
+                <span>-</span>
+                <p>KEPALA SEKSI PEMERINTAHAN DAN TRANTIB UMUM</p>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="fade-up" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Syane Menda, A.Md</h4>
+                <span>19780928 201001 2 001</span>
+                <p>KEPALA SEKSI EKONOMI DAN PEMBANGUNAN</p>
+                <div class="social">
+                  </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="fade-up" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>-</h4>
+                <span>-</span>
+                <p>KEPALA SEKSI PELAYANAN UMUM DAN KESEJAHTERAAN SOSIAL</p>
+                <div class="social">
+                  </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-6 mt-4" data-aos="fade-up" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Fadly Basalamah, SE</h4>
+                <span>19751003 201001 1 005<span>
+                <h4>Debi Takahindangen, S.Pd</h4>
+                <p>STAF</span>
+                <div class="social">
+                  </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="fade-up" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Sarni Makasenda</h4>
+                <p>STAF</span>
+                <div class="social">
+                  </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="fade-up" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+             <div class="member-info">
+                <h4>Yustuti Gamis</h4>
+                <p>NIP. 19860709 201407 2 003</span>
+                <p>PELAKSANA TUGAS</span>
+                <div class="social">
+                  </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section><!-- End Team Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    </section><!-- End Pricing Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+
+       </div>
+    </section><!-- End Frequently Asked Questions Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Hubungi Kami</h2>
+        </div>
+
+        <div class="row mt-1 d-flex center-content-end" data-aos="fade-right" data-aos-delay="100">
+
+          <div class="col-lg-5">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Lokasi:</h4>
+                <p>Tahuna, Kabupaten Kepulauan Sangihe Sulawesi Utara</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email Kantor:</h4>
+                <p>info@example.com</p>
+              </div>
+            </div>
+          </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            </div>
+          </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Mahasiswa KKL 2021</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade/Imes</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
